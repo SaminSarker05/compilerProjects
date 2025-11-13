631be49b837b14a8984c8e77e152b68a6eaf4961
Samin Sarker <saminsarker05@gmail.com>
2025-11-13 12:29:36 -0500

    adding interpreter

---
+++ monkey_Interpreter/token/token.go
@@ -0,0 +1,31 @@
+package token
+
+type TokenType string
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENTIFIER = "IDENTIFIER"
+	INT        = "INT"
+
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
