6f6d6c1f650b3d7055ab945981a42c994c0adce7
Samin Sarker <saminsarker05@gmail.com>
2025-11-20 16:00:06 -0500

    more progress defined codegen for for methodcallnode

--- mango_compiler/generator.cpp
+++ mango_compiler/generator.cpp
@@ -22,7 +22,8 @@ void GenCodeContext::generate_code(BlockNode& root) {
     std::vector<llvm::Type*> arg_types;
     llvm::FunctionType* ftype = FunctionType::get(Type::getVoidTy(*context), arg_types, false);
     // define actual main function, visible only within module
-    main_func = llvm::Function::Create(ftype, llvm::GlobalValue::InternalLinkage, "main", module.get());
+    main_func = llvm::Function::Create(ftype, llvm::GlobalValue::InternalLinkage, 
+        "main", module.get());
 
     // add first basic block and start code generation
     llvm::BasicBlock* bb = llvm::BasicBlock::Create(*context, "entry", main_func);
@@ -57,5 +58,66 @@ llvm::GenericValue GenCodeContext::run_code() {
     return result;
 }
 
+llvm::Value* IntegerNode::codeGen(GenCodeContext& context) {
+    std::cout << "IntegerNode::codeGen" << std::endl;
+    // create llvm 64 bit signed integer constant
+    return llvm::ConstantInt::get(llvm::Type::getInt64Ty(*context.context), value, true);
+}
 
+llvm::Value* DoubleNode::codeGen(GenCodeContext& context) {
+    std::cout << "DoubleNode::codeGen" << std::endl;
+    // create llvm 64 bit double constant
+    return llvm::ConstantFP::get(llvm::Type::getDoubleTy(*context.context), value);
+}
 
+llvm::Value* IdentifierNode::codeGen(GenCodeContext& context) {
+    std::cout << "IdentifierNode::codeGen" << std::endl;
+    // lookup variable in symbol table
+    if (context.get_locals().find(name) == context.get_locals().end()) {
+        std::cerr << "Undefined variable: " << name << std::endl;
+        return nullptr;
+    }
+
+    // load variable from memory
+    return context.builder->CreateLoad(llvm::Type::getInt64Ty(*context.context), 
+        context.get_locals()[name], name.c_str());
+}
+
+llvm::Value* AssignmentNode::codeGen(GenCodeContext& context) {
+    
+}
+
+llvm::Value* MethodCallNode::codeGen(GenCodeContext& context) {
+    llvm::Function* func = context.module->getFunction(name);
+    if (func == nullptr) {
+        std::cerr << "Undefined function: " << name << std::endl;
+        return nullptr;
+    }
+    // generate code for each expression in argument list
+    std::vector<llvm::Value*> arg_values;
+    for (ExprNode* arg : args) {
+        args.push_back(arg->codeGen(context));
+    }
+    // create a call instruction
+    return context.builder->CreateCall(func, arg_values, "calldebug");
+}
+
+llvm::Value* BinaryOpNode::codeGen(GenCodeContext& context) {
+    
+}
+
+llvm::Value* BlockNode::codeGen(GenCodeContext& context) {
+    
+}
+
+llvm::Value* ExprStmtNode::codeGen(GenCodeContext& context) {
+    
+}
+
+llvm::Value* FuncDeclNode::codeGen(GenCodeContext& context) {
+    
+}
+
+llvm::Value* VarDeclNode::codeGen(GenCodeContext& context) {
+    
+}
